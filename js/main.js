558e338cdf3c66244c5edd712880432e65d7be3e
Hudson Stevens <hudsonstevens@Hudsons-MacBook-Air.local>
2025-11-23 19:45:45 -0800

    Add initial website files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("Physics Website Loaded");
+let x = 50;
+let y = 50;
+let speedX = 3;
+let speedY = 2;
+
+function setup() {
+  createCanvas(400, 300);
+}
+
+function draw() {
+  background(220);
+  ellipse(x, y, 30, 30);
+
+  x += speedX;
+  y += speedY;
+
+  if (x > width || x < 0) speedX *= -1;
+  if (y > height || y < 0) speedY *= -1;
+}
+
